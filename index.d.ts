17253a0d0c1b9c4b7526af3dd4da5f35c77550f1
Ali Zahid <ali.zahid@live.com>
2020-11-01 23:12:12 +0500

    Refactoring

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,8 @@
 import Prisma from '@prisma/client'
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import * as auth from 'next-auth'
 
 declare module 'next-auth' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-interface
   interface User extends Prisma.User {}
 }
