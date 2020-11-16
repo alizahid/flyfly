31ebae34310aab88e7f29e386f04aedd37f5da94
Ali Zahid <ali.zahid@live.com>
2020-11-16 19:29:25 +0500

    Indexes added

--- server/db/index.ts
+++ server/db/index.ts
@@ -6,11 +6,57 @@ export const client = new MongoClient(process.env.MONGO_URI, {
 })
 
 export const mongo = async (): Promise<Db> => {
+  let db: Db
+
   if (!client.isConnected()) {
     await client.connect()
+
+    db = client.db()
+
+    // user
+
+    db.collection('users').createIndex(
+      {
+        email: 1
+      },
+      {
+        unique: true
+      }
+    )
+
+    // project
+
+    db.collection('projects').createIndex({
+      userId: 1
+    })
+
+    // form
+
+    db.collection('forms').createIndex({
+      projectId: 1,
+      userId: 1
+    })
+
+    db.collection('forms').createIndex(
+      {
+        slug: 1
+      },
+      {
+        unique: true
+      }
+    )
+
+    // response
+
+    db.collection('responses').createIndex({
+      formId: 1,
+      userId: 1
+    })
+  } else {
+    db = client.db()
   }
 
-  return client.db()
+  return db
 }
 
 export * from './form'
