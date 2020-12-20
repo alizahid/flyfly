795adb174ecb72732316b21d77a1a4d795816e2d
Ali Zahid <ali.zahid@live.com>
2020-12-20 16:29:40 +0400

    Refactoring

--- components/index.ts
+++ components/index.ts
@@ -1,14 +1,22 @@
+// common
+
 export { Code, InlineCode } from './code'
 export { Footer } from './footer'
-export { FormCard } from './forms/card'
 export { GetStarted } from './get-started'
 export { Header } from './header'
 export { Icon } from './icon'
 export { Loading } from './loading'
 export { Modal } from './modal'
+export { Spinner } from './spinner'
+
+// app
+
+export { FormCard } from './forms/card'
 export { ProjectCard } from './projects/card'
 export { ResponseCard } from './responses/card'
-export { Spinner } from './spinner'
+
+// account
+
 export { NotificationsCard } from './user/notifications'
 export { PlanCard } from './user/plan'
 export { ProfileCard } from './user/profile'
