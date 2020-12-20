fc90505bbe8018b9c6810a3f3275df8c93765891
Ali Zahid <ali.zahid@live.com>
2020-12-20 16:28:55 +0400

    Refactoring

--- components/user/profile.tsx
+++ components/user/profile.tsx
@@ -13,26 +13,24 @@ export const ProfileCard: FunctionComponent<Props> = ({
   className,
   profile
 }) => (
-  <>
-    <div
-      className={`bg-white shadow-sm rounded-xl p-8 lg:p-4 flex flex-col lg:flex-row items-center ${className}`}>
-      <img
-        className="bg-emerald-200 h-20 w-20 rounded-full shadow-sm"
-        src={profile.image}
-      />
-      <div className="my-4 lg:my-0 lg:mx-8 text-center lg:text-left">
-        <div className="text-xl font-medium">{profile.name}</div>
-        <div className={profile.email ? '' : 'text-gray-500'}>
-          {profile.email || 'No email provided'}
-        </div>
-      </div>
-      <div className="flex items-center">
-        <Icon
-          color={profile.verified ? 'green' : 'gray'}
-          icon="shieldCheckmark"
-          title={profile.verified ? 'Email verified' : 'Email not verified'}
-        />
+  <div
+    className={`bg-white shadow-sm rounded-xl p-8 lg:p-4 flex flex-col lg:flex-row items-center ${className}`}>
+    <img
+      className="bg-emerald-200 h-20 w-20 rounded-full shadow-sm"
+      src={profile.image}
+    />
+    <div className="my-4 lg:my-0 lg:mx-8 text-center lg:text-left">
+      <div className="text-xl font-medium">{profile.name}</div>
+      <div className={profile.email ? '' : 'text-gray-500'}>
+        {profile.email || 'No email provided'}
       </div>
     </div>
-  </>
+    <div className="flex items-center">
+      <Icon
+        color={profile.verified ? 'green' : 'gray'}
+        icon="shieldCheckmark"
+        title={profile.verified ? 'Email verified' : 'Email not verified'}
+      />
+    </div>
+  </div>
 )
